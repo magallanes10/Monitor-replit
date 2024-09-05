b314429e84d6d38f76d09dfb6077193b6ba9e932
Jonell Magallanes <125812726+magallanes10@users.noreply.github.com>
2024-09-05 23:01:02 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const express = require("express");
 const { scrapeLogic } = require("./scrapeLogic");
-const path = require("path");
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 
-app.get("/scrape", (req, res) => {
-  scrapeLogic(res, false); // Normal scraping without screenshot
-});
-
 app.get("/ss", (req, res) => {
-  scrapeLogic(res, true); // Scraping with screenshot response
+  const url = req.query.url;
+  const device = req.query.device || 'desktop';
+
+  if (!url) {
+    return res.send("Error: URL is required.");
+  }
+
+  scrapeLogic(res, url, device);
 });
 
 app.get("/", (req, res) => {
-  res.send("Render Puppeteer server is up and running!");
+  res.send("Puppeteer server is up and running!");
 });
 
 app.listen(PORT, () => {
