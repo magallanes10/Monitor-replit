3f0ff17fbb46c96ef218faa1101fd220dbb51512
Jonell Magallanes <125812726+magallanes10@users.noreply.github.com>
2024-09-05 22:21:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const { scrapeLogic } = require("./scrapeLogic");
+const path = require("path");
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 
 app.get("/scrape", (req, res) => {
-  scrapeLogic(res);
+  scrapeLogic(res, false); // Normal scraping without screenshot
+});
+
+app.get("/ss", (req, res) => {
+  scrapeLogic(res, true); // Scraping with screenshot response
 });
 
 app.get("/", (req, res) => {
